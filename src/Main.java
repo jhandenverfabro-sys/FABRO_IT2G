085bb908e2c959384151202732dd0c9837f90379
jhandenverfabro-sys <jhandenverfabro@gmail.com>
2026-08-27 15:25:37 +0800

    activity2

--- src/Main.java
+++ src/Main.java
@@ -7,15 +7,22 @@ public static void main (String[] args){
     System.out.println("SECTION AND YEAR: IT2G");
     System.out.println("---------CONTENT---------");
     System.out.println("1. Activity 1");
+    System.out.println("2. Activity 2");
+    System.out.println("3. Activity 3");
     System.out.println("-------------------------");
     System.out.println("Enter Selection");
     int select = sc.nextInt();
     System.out.println("-------------------------");
 
-        if(select == 1){
-            activity1 act1 = new activity1();
-            act1.userGreetigs();
-        }else{
-            System.out.println("Selection Not Found!");
-        }
+    if (select == 1) {
+        activity1 act1 = new activity1();
+        act1.userGreetigs();
+
+    } else if (select == 2) {
+        activity2 act2 = new activity2();
+        act2.cinemaTicket();
+    } else {
+        System.out.println("Selection Not Found!");
+    }
+
 }
